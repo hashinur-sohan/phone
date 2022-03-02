86fd0ae659f1cce2631e1d34c4f2a2d146eea0ec
Md Hashinur Rashid Sohan <m0shn@outlook.com>
2022-03-02 22:26:10 +0600

    phone search done

--- js/api.js
+++ js/api.js
@@ -7,7 +7,6 @@ function inputName() {
 document.getElementById('button-addon2').addEventListener('click', function () {
 
     const returnInput = inputName();
-    console.log(returnInput);
     fetch(`https://openapi.programming-hero.com/api/phones?search=${returnInput}`)
         .then(response => response.json())
         // .then(json => console.log(json))
@@ -16,25 +15,32 @@ document.getElementById('button-addon2').addEventListener('click', function () {
 
 })
 
+function detailse(data) {
+    fetch(`https://openapi.programming-hero.com/api/phone/${data}`)
+        .then(response => response.json())
+        //.then(json => console.log(json))
+        .then(data => displayPhone(data))
+    console.log(data);
+}
+
 function displayPhone(data) {
     // const phone = phones.brand;
-    const ul = document.getElementById('ul');
+    const cardId = document.getElementById('card-id');
     for (const user of data) {
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
-        
-                <div class="card">
-                    <img src=${"user.image"} class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${user.name}</h5>
-                        <p class="card-text">${user.brand}</p>
-                    </div>
-                </div>
-            
+            <div class="card">
+                 <img src=${user.image} class="card-img-top w-50 mx-auto" alt="...">
+                <div class="card-body">
+                     <h5 class="card-title">${user.phone_name}</h5>
+                      <p class="card-text">${user.brand}</p>
+                      <button onclick="detailse  ('${user.slug}')">Details</button>
+                 </div>
+            </div>  
         `
+        cardId.appendChild(div);
     }
-
 }
 
 
